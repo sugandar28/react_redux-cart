491825ea38c4f1274d679a3443e4ce2a8a655643
Sugandar <sugandar@ozzi.tech>
2020-06-01 21:46:00 +0530

    animation added

--- src/component/Products.js
+++ src/component/Products.js
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
 import formateCurrency from "../util";
+import Fade from "react-reveal/Fade";
 
 export default class Products extends Component {
   render() {
     return (
       <div>
-        <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <a href={"#" + product._id}>
-                  <img src={product.image} alt={product.title} />
-                  <p>{product.title}</p>
-                  <div className="product_price">
-                    <div>{formateCurrency(product.price)}</div>
-                    <button
-                      onClick={() => this.props.addToCart(product)}
-                      className="button primary"
-                    >
-                      {" "}
-                      Add To cart
-                    </button>
-                  </div>
-                </a>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <Fade bottom cascade>
+          <ul className="products">
+            {this.props.products.map((product) => (
+              <li key={product._id}>
+                <div className="product">
+                  <a href={"#" + product._id}>
+                    <img src={product.image} alt={product.title} />
+                    <p>{product.title}</p>
+                    <div className="product_price">
+                      <div>{formateCurrency(product.price)}</div>
+                      <button
+                        onClick={() => this.props.addToCart(product)}
+                        className="button primary"
+                      >
+                        {" "}
+                        Add To cart
+                      </button>
+                    </div>
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Fade>
       </div>
     );
   }
